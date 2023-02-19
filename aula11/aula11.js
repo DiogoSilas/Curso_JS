cb749501444fddd386ff95889366052d958bb82e
Diogo Silas Mendes Ribeiro <108630083+DiogoSilas@users.noreply.github.com>
2023-02-19 17:33:16 -0300

    Aula 11

---
+++ aula11/aula11.js
@@ -0,0 +1,7 @@
+var vel = 61
+
+window.document.writeln(`A velocidade do seu automovel é de ${vel}Km/h.<br>`)
+if (vel > 60) {
+    window.document.writeln(`Você ultrapassou a velocidade permitida, MULTADO.<br>`)
+}
+window.document.writeln(`Dirija sempre com o sinto de segurança!.`)
